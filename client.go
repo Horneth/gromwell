a75c44f7b14f172b35e5df01a93b56b343e4a762
Thibault Jeandet <tjeandet@broadinstitute.org>
2017-05-26 18:15:47 -0400

    fix

--- client.go
+++ client.go
@@ -6,6 +6,11 @@ import (
 	"errors"
 	"io/ioutil"
 	"strings"
+	"io"
+	"os"
+	"fmt"
+	"bytes"
+	"mime/multipart"
 )
 
 const engineEndpoint = "/api/engine/v1"
@@ -17,13 +22,13 @@ const outputsEndpoint = "/outputs"
 const metadataEndpoint = "/metadata"
 const abortEndpoint = "/abort"
 
-func (client CromwellClient) AbortWorkflow(workflowId string) (*WorkflowStatus, error) {
-	var status *WorkflowStatus
+func (client CromwellClient) AbortWorkflow(workflowId string) (WorkflowStatus, error) {
+	var status WorkflowStatus
 	body, err := getResponseBody(http.Post(client.makeWorkflowEndpointPath(workflowId, abortEndpoint), "text/plain", strings.NewReader("")))
 	if (err != nil) {
 		return status, err
 	}
-	err = json.Unmarshal(body, status)
+	err = json.Unmarshal(body, &status)
 	if (err != nil) {
 		return status, err
 	}
@@ -40,41 +45,42 @@ func (client CromwellClient) Version() (string, error) {
 	return version, err
 }
 
-func (client CromwellClient) SubmitWorkflow(command SubmitCommand) (*WorkflowStatus, error) {
-	var status *WorkflowStatus
+func (client CromwellClient) SubmitWorkflow(command SubmitCommand) (WorkflowStatus, error) {
+	var status WorkflowStatus
+	
 	body, err := getResponseBody(client.submit(command))
 	if (err != nil) {
 		return status, err
 	}
-	err = json.Unmarshal(body, status)
+	err = json.Unmarshal(body, &status)
 	if (err != nil) {
 		return status, err
 	}
 	return status, err
 }
 
-func (client CromwellClient) GetWorkflowStatus(workflowId string) (*WorkflowStatus, error) {
-	var status *WorkflowStatus
+func (client CromwellClient) GetWorkflowStatus(workflowId string) (WorkflowStatus, error) {
+	var status WorkflowStatus
 	body, err := getResponseBody(http.Get(client.makeWorkflowEndpointPath(workflowId, statusEndpoint)))
 	if (err != nil) {
 		return status, err
 	}
-	err = json.Unmarshal(body, status)
+	err = json.Unmarshal(body, &status)
 	if (err != nil) {
 		return status, err
 	}
 	return status, err
 }
 
-func (client CromwellClient) GetWorkflowOutputs(workflowId string) (*WorkflowOutputs, error) {
-	var outputs *WorkflowOutputs
+func (client CromwellClient) GetWorkflowOutputs(workflowId string) (WorkflowOutputs, error) {
+	var outputs WorkflowOutputs
 
 	// Get Body
 	body, err := getResponseBody(http.Get(client.makeWorkflowEndpointPath(workflowId, outputsEndpoint)))
 	if (err != nil) { return outputs, err }
 
 	// Construct response
-	outputs = &WorkflowOutputs {
+	outputs = WorkflowOutputs {
 		JsonResponse: &JsonResponse {
 			Id: workflowId,
 			JsonValue: body,
@@ -84,15 +90,15 @@ func (client CromwellClient) GetWorkflowOutputs(workflowId string) (*WorkflowOut
 	return outputs, err
 }
 
-func (client CromwellClient) GetWorkflowMetadata(workflowId string) (*WorkflowMetadata, error) {
-	var metadata *WorkflowMetadata
+func (client CromwellClient) GetWorkflowMetadata(workflowId string) (WorkflowMetadata, error) {
+	var metadata WorkflowMetadata
 
 	// Get Body
 	body, err := getResponseBody(http.Get(client.makeWorkflowEndpointPath(workflowId, metadataEndpoint)))
 	if (err != nil) { return metadata, err }
 
 	// Construct response
-	metadata = &WorkflowMetadata {
+	metadata = WorkflowMetadata {
 		JsonResponse: &JsonResponse {
 			Id: workflowId,
 			JsonValue: body,
@@ -108,9 +114,75 @@ func getResponseBody(resp *http.Response, err error) ([]byte, error) {
 	if (err != nil) {
 		return body, err
 	}
-	if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
-		return body, errors.New("Status query failed: " + resp.Status)
+	if (resp.StatusCode >= 300) {
+		return body, errors.New("Request failed: " + resp.Status)
 	}
 	body, err = ioutil.ReadAll(resp.Body)
 	return body, err
+}
+
+
+func (client CromwellClient) makeWorkflowEndpointPath(workflowId string, endpoint string) string {
+	return fmt.Sprintf("%s%s/%s%s", client.CromwellUrl.String(), workflowEndpoint, workflowId, endpoint)
+}
+
+func (client CromwellClient) makeEngineEndpointPath(endpoint string) string {
+	return fmt.Sprintf("%s%s%s", client.CromwellUrl.String(), engineEndpoint, endpoint)
+}
+
+func (client CromwellClient) submitEndpoint() string {
+	return client.CromwellUrl.String() + workflowEndpoint
+}
+
+func (cromwellClient CromwellClient) submit(command SubmitCommand) (*http.Response, error) {
+	wdlSource := command.WdlSource
+	workflowInputs := command.WorkflowInputs
+	workflowOptions := command.WorkflowOptions
+
+	// buffer that'll contain the form content
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	// Open WDL file
+	wdlFile, err := os.Open(wdlSource)
+	if err != nil { return nil, err }
+	defer wdlFile.Close()
+
+	fw, err := w.CreateFormFile("wdlSource", wdlSource)
+	if err != nil { return nil, err	}
+	if _, err = io.Copy(fw, wdlFile); err != nil { return nil, err }
+
+	if (workflowInputs != "") {
+		inputsFile, err := os.Open(workflowInputs)
+		if err != nil { return nil, err }
+		defer inputsFile.Close()
+
+		fw, err := w.CreateFormFile("workflowInputs", workflowInputs)
+		if err != nil { return nil, err	}
+		if _, err = io.Copy(fw, inputsFile); err != nil { return nil, err }
+	}
+
+	if (workflowOptions != "") {
+		optionsFile, err := os.Open(workflowOptions)
+		if err != nil { return nil, err }
+		defer optionsFile.Close()
+
+		fw, err := w.CreateFormFile("workflowOptions", workflowOptions)
+		if err != nil { return nil, err	}
+		if _, err = io.Copy(fw, optionsFile); err != nil { return nil, err }
+	}
+
+	w.Close()
+
+	// Now that you have a form, you can submit it to your handler.
+	req, err := http.NewRequest("POST", cromwellClient.submitEndpoint(), &b)
+	if err != nil {	return nil, err	}
+
+	req.Header.Set("Content-Type", w.FormDataContentType())
+
+	// Submit the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	return res, err
 }
--- model.go
+++ model.go
@@ -3,81 +3,12 @@ package gromwell
 import (
 	"net/url"
 	"fmt"
-	"os"
-	"io"
-	"net/http"
-	"bytes"
-	"mime/multipart"
 	"encoding/json"
+	"io/ioutil"
 )
 
 type CromwellClient struct {
 	CromwellUrl *url.URL
-}
-
-func (client CromwellClient) makeWorkflowEndpointPath(workflowId string, endpoint string) string {
-	return fmt.Sprintf("%s%s/%s%s", client.CromwellUrl.String(), workflowEndpoint, workflowId, endpoint)
-}
-
-func (client CromwellClient) makeEngineEndpointPath(endpoint string) string {
-	return fmt.Sprintf("%s%s%s", client.CromwellUrl.String(), engineEndpoint, endpoint)
-}
-
-func (client CromwellClient) submitEndpoint() string {
-	return client.CromwellUrl.String() + workflowEndpoint
-}
-
-func (cromwellClient CromwellClient) submit(command SubmitCommand) (*http.Response, error) {
-	wdlSource := command.WdlSource
-	workflowInputs := command.WorkflowInputs
-	workflowOptions := command.WorkflowOptions
-
-	// buffer that'll contain the form content
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-
-	// Open WDL file
-	wdlFile, err := os.Open(wdlSource)
-	if err != nil { return nil, err }
-	defer wdlFile.Close()
-
-	fw, err := w.CreateFormFile("wdlSource", wdlSource)
-	if err != nil { return nil, err	}
-	if _, err = io.Copy(fw, wdlFile); err != nil { return nil, err }
-
-	if (workflowInputs != "") {
-		inputsFile, err := os.Open(workflowInputs)
-		if err != nil { return nil, err }
-		defer inputsFile.Close()
-
-		fw, err := w.CreateFormFile("workflowInputs", workflowInputs)
-		if err != nil { return nil, err	}
-		if _, err = io.Copy(fw, inputsFile); err != nil { return nil, err }
-	}
-
-	if (workflowOptions != "") {
-		optionsFile, err := os.Open(workflowOptions)
-		if err != nil { return nil, err }
-		defer optionsFile.Close()
-
-		fw, err := w.CreateFormFile("workflowOptions", workflowOptions)
-		if err != nil { return nil, err	}
-		if _, err = io.Copy(fw, optionsFile); err != nil { return nil, err }
-	}
-
-	w.Close()
-
-	// Now that you have a form, you can submit it to your handler.
-	req, err := http.NewRequest("POST", cromwellClient.submitEndpoint(), &b)
-	if err != nil {	return nil, err	}
-
-	req.Header.Set("Content-Type", w.FormDataContentType())
-
-	// Submit the request
-	client := &http.Client{}
-	res, err := client.Do(req)
-
-	return res, err
 }
 
 // Requests
@@ -126,6 +57,10 @@ func (status WorkflowStatus) String() string {
 
 func (jsonResponse *JsonResponse) toJson() (map[string]interface{}, error) {
 	return parseJson(jsonResponse.JsonValue)
+}
+
+func (jsonResponse *JsonResponse) ToFile(filePath string) error {
+	return ioutil.WriteFile(filePath, jsonResponse.JsonValue, 0644)
 }
 
 func prettyPrint(data interface{}) string {
